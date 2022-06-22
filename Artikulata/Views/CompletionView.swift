052d6a8e09a4b6a09f79063f062cc866bacf45fa
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2022-06-22 21:07:09 +0700

    Add correct/wrong logic for speech recognition

---
+++ Artikulata/Views/CompletionView.swift
@@ -0,0 +1,20 @@
+//
+//  CompletionView.swift
+//  Artikulata
+//
+//  Created by Ryan Vieri Kwa on 22/06/22.
+//
+
+import SwiftUI
+
+struct CompletionView: View {
+    var body: some View {
+        Text("Kamu Hebat!")
+    }
+}
+
+struct CompletionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompletionView()
+    }
+}
