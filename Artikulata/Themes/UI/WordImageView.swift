11220910bf089435c0724b06e4c8fd00d5a9ef9c
mina0110 <minawati10@gmail.com>
2022-06-22 12:05:32 +0700

    add video-speech UI and list of words navigation

---
+++ Artikulata/Themes/UI/WordImageView.swift
@@ -0,0 +1,45 @@
+//
+//  WordImageView.swift
+//  Artikulata
+//
+//  Created by Minawati on 14/06/22.
+//
+
+import SwiftUI
+
+struct WordImageView: View {
+    
+    let imageName: String
+    let imageText: String
+    
+    @State var isActive = false
+    var word: Video
+    
+    var body: some View {
+            // masih perlu ditambahin isPressed event
+            // bisa dikasih animation(.spring())
+            Button {
+                self.isActive = true
+            } label: {
+                VStack{
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150, alignment: .center)
+                    FontView(text: imageText, size: 26)
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
+            .background(
+                NavigationLink(destination: VideoSpeechView(video: word).navigationBarBackButtonHidden(true), isActive: $isActive) {
+                }
+                .hidden()
+            )
+    }
+}
+
+struct WordImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        WordImageView(imageName: "Senang", imageText: "Senang", word: Video.Senang)
+    }
+}
---
+++ Artikulata/Views/ListWordView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Artikulata
+//
+//  Created by Ryan Vieri Kwa on 08/06/22.
+//
+
+import SwiftUI
+
+struct ListWordView: View {
+    @State var isActive: Bool = false
+    @State var word: String = "Marah"
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                
+                Image("BG-kata-sifat")
+                    .resizable()
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                VStack {
+                    
+                    HStack {
+                        
+                        BackButtonView()
+                        
+                        Spacer()
+                        
+                        FontView(text: "Kata Sifat", size: 32)
+                            .frame(width: 210, height: 0, alignment: .leading)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack(spacing: 100) {
+                        WordImageView(imageName: "Senang", imageText: "Senang", word: Video.Senang)
+                        
+                        WordImageView(imageName: "Marah", imageText: "Marah", word: Video.Marah)
+                        
+                        WordImageView(imageName: "Sedih", imageText: "Sedih", word: Video.Sedih)
+                    }
+                    .padding(.top, 40)
+                }
+                .padding(.bottom, 40)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListWordView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext).previewInterfaceOrientation(.landscapeRight)
+    }
+}
